b7517c33b4c15053509ffc4dd322e19da89b7adf
darlandieterich <darlandieterich@gmail.com>
2020-07-18 00:22:16 -0300

    first of all

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RetroConsole output="blash main consolO"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RetroConsole from './components/RetroConsole.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RetroConsole
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/RetroConsole.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="retro-console">
+    {{output}}    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RetroConsole',
+  props: {
+    output: String
+  }
+}
+</script>
+
+<style scoped>
+.retro-console {
+  color: #2c3e50;
+}
+</style>
