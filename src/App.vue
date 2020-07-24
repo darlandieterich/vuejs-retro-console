660c943ebc8c9f357dff79b64bb78e4c7fdcb7c5
darlandieterich <darlandieterich@gmail.com>
2020-07-23 22:08:37 -0300

    Update path. Prepare for build

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <input type="text" ref="fieldx" v-model="value" v-on:keyup.enter="insere">
-    <RetroConsole :output="lines" :fullscreen="false" :fontSize="15" :consoleSize="{height:'400px',width:'50%'}"/>
+    <RetroConsole :output="lines" :fullscreen="false" :fontSize="15" :consoleSize="{height:'100px',width:'50%'}"/>
   </div>
 </template>
 
 <script>
-import RetroConsole from './components/RetroConsole.vue'
+import RetroConsole from './RetroConsole'
 
 export default {
   name: 'App',
